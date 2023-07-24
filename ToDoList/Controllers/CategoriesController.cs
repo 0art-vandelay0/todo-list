2965ca03850dec1d6383666db4dc376439730efd
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-07-24 09:34:33 -0700

    Add files

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 
@@ -11,6 +10,24 @@ namespace ToDoList.Controllers
         public ActionResult New()
         {
             return View();
+        }
+
+        [HttpPost("/categories")]
+        public ActionResult Create(string categoryName)
+        {
+            Category newCategory = new Category(categoryName);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("/categories/{categoryId}/items/{itemId}")]
+        public ActionResult Show(int categoryId, int itemId)
+        {
+            Item item = Item.Find(itemId);
+            Category category = Category.Find(categoryId);
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            model.Add("item", item);
+            model.Add("category", category);
+            return View(model);
         }
     }
 }
