eaa05a77b2cbc0d6747444ebb27e257b357e43f9
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-08-07 10:45:57 -0700

    Edit default settings

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -27,6 +27,28 @@ namespace ToDoList
                             .AddEntityFrameworkStores<ToDoListContext>()
                             .AddDefaultTokenProviders();
 
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                // Default Password settings.
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 0;
+                options.Password.RequiredUniqueChars = 0;
+            });
+
+            // builder.Services.Configure<IdentityOptions>(options =>
+            // {
+            //   // Default Password settings.
+            //   options.Password.RequireDigit = true;
+            //   options.Password.RequireLowercase = true;
+            //   options.Password.RequireNonAlphanumeric = true;
+            //   options.Password.RequireUppercase = true;
+            //   options.Password.RequiredLength = 6;
+            //   options.Password.RequiredUniqueChars = 1;
+            // });
+
             WebApplication app = builder.Build();
 
             // app.UseDeveloperExceptionPage();
